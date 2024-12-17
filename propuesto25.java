ac2a5242fd8b35294a56fc513435e55a8fcf459e
sergi1307 <sergi13072006@gmail.com>
2024-12-17 13:51:57 +0100

    Añadimos propuesto 25 terminado, y con esto todos los propuestos II

--- propuesto25.java
+++ propuesto25.java
@@ -1,3 +1,25 @@
-public class propuesto25 {
+import java.util.Scanner;
 
+public class propuesto25 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int lineas;
+        int asteriscos = 1;
+
+        System.out.println("Introduce el número de veces que se imprimirá el (*): ");
+        lineas = teclado.nextInt();teclado.nextLine();
+
+        for (int i = 1; i <= lineas; i++) {
+            int espacios = lineas - (asteriscos / 2);
+            for (int j = 1; j <= espacios; j++){
+                System.out.print(" ");
+            }
+            for (int x = 1; x <= asteriscos; x++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            asteriscos += 2;
+        }
+    }
 }
